9ef3e1b44a44ebace49006b1c88e39725cd06f7e
SIDHARTHA-DAS <sidharthadas963@gmail.com>
2026-04-06 09:39:17 +0530

    add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
   return (
     <>
-       <div className='text-center p-5 bg-blue-900 text-2xl'><Link to="/">Media Search</Link></div>
+      <div className='text-center p-5 bg-blue-900 text-2xl'><Link to="/">Media Search</Link></div>
       <div className='gap-10 flex justify-center text-xl'>  
       <Link className='text-xl bg-green-600 m-4 p-2 rounded active:scale-95 font-medium' to={"/"}>Search</Link>
       <Link className='text-xl bg-green-600 m-4 p-2 rounded active:scale-95 font-medium' to={"/collection"}>Collection</Link>
